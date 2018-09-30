0bad7480914ba1193950101d8303eabc3abf92f6
Gabriel Delfino <gabrielmoysesdelfino@hotmail.com>
2018-09-29 22:52:19 -0300

    Identação_2

--- Esfera_vista_frontal.js
+++ Esfera_vista_frontal.js
@@ -6,7 +6,7 @@ function draw() {
 	Numero_pontos = 200;
 	tamanho =3;
 	tamanho_centro = 8;
-  centro = 200;
+  	centro = 200;
 	raio = 100;
 	a=0;
 	background(140);
@@ -16,13 +16,12 @@ function draw() {
 	ellipse(centro, centro,tamanho_centro, tamanho_centro);
 	
 	//Criando a esfera
-  for(c=0;c<20;c++){	
-    raio= raio - 5;
-    for(b=0;b<Numero_pontos;b++){		
-		fill(0, 255, 0, 255);
-		ellipse(centro + raio*cos(a), centro + raio*sin(a), tamanho, tamanho);
-		a=a+PI/20;
-	}
-  }
-	
+  	for(c=0;c<20;c++){	
+    		raio= raio - 5;
+    		for(b=0;b<Numero_pontos;b++){		
+			fill(0, 255, 0, 255);
+			ellipse(centro + raio*cos(a), centro + raio*sin(a), tamanho, tamanho);
+			a=a+PI/20;
+		}
+  	}
 }
