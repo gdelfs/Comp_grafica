293d444fe96e7c26d139480e098d0521ba26192d
Gabriel Delfino <gabrielmoysesdelfino@hotmail.com>
2018-09-29 22:00:56 -0300

    Create Vista_frontal.js

---
+++ Vista_frontal.js
@@ -0,0 +1,33 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+	Numero_pontos = 200;
+	tamanho =3;
+	tamanho_centro = 8;
+  centro = 200;
+	raio = 100;
+	a=0;
+	background(140);
+	
+	//Centro da esfera
+	fill(255, 0, 0, 255);
+	ellipse(centro, centro,tamanho_centro, tamanho_centro);
+  	//Criando o círculo
+	for(b=0;b<Numero_pontos;b++){		
+		fill(255, 255, 0, 255);
+		ellipse(centro + raio*cos(a), centro + raio*sin(a), tamanho, tamanho);
+		a=a+PI/50;
+	}
+	//Criando a esfera
+  for(c=0;c<20;c++){	
+    raio= raio - 5;
+    for(b=0;b<Numero_pontos;b++){		
+		fill(0, 255, 0, 255);
+		ellipse(centro + raio*cos(a), centro + raio*sin(a), tamanho, tamanho);
+		a=a+PI/20;
+	}
+  }
+	
+}
